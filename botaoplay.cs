ea86bea67fef6171da8c3dc89558af07345b1101
André Judá <judatrainer@gmail.com>
2024-03-09 11:46:04 -0300

    first commit

---
+++ botaoplay.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Botaoplay : MonoBehaviour
+{
+
+    public void Carregarlvl1()
+    {
+        //Debug.Log("Botao foi apertado");
+        SceneManager.LoadSceneAsync("cenalevel1");
+    }
+
+   
+}
